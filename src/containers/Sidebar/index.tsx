2b98ddf909d932e03cf0822c8124dbaf5394c9e0
AndersonTemoteo <andersontemoteobezerra@gmail.com>
2024-06-05 18:46:21 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/AndersonTemoteo.png" />
+    <Titulo fontSize={20}>Anderson Temoteo</Titulo>
+  </aside>
+)
+
+export default Sidebar
