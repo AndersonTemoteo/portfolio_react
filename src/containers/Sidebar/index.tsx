f33b601a2235918516a925c911a3e54c5830eec0
AndersonTemoteo <andersontemoteobezerra@gmail.com>
2024-06-05 22:51:25 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avata'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SiderbarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/AndersonTemoteo.png" />
-    <Titulo fontSize={20}>Anderson Temoteo</Titulo>
+    <SiderbarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Anderson Temoteo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        AndersonTemoteo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro fron-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SiderbarContainer>
   </aside>
 )
 
